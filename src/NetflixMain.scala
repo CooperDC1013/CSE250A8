93c7913e3c22c0740194e87fa6443b61d02ace86
Michael <msmcdona@buffalo.edu>
2022-05-12 22:34:17 -0400

    Various tweaks. Added comments, shifted ordering, changed output format

--- src/NetflixMain.scala
+++ src/NetflixMain.scala
@@ -1,74 +1,79 @@
-object NetflixMain extends App {
+import java.io.FileWriter
 
-  def keyComp(score1: Double, score2: Double): Int = {
-    if (score1 > score2) return -1
-    else if (score1 == score2) return 0
-    1
+object NetflixMain extends App {
+  // Allows an optional command line argument for the number of recommendations provided
+  private val number_of_recommendations: Int = if (args.length == 1) args(0).toInt else 30
+
+  // Heap for storing each user's recommendations; a new heap is used for each user
+  private class Recommendations extends Heap[(String, Double)](1682, (x, y) => x._2.compareTo(y._2))
+
+  private val moviebase = new MovieBaseCDC
+  private val userbase = new Userbase_MM
+
+  private val allMovies = moviebase.getMovies
+  private val allUsers = userbase.get_all_users()
+
+  // Accumulates the average rating of all movies for each genre; used to calculate p_ug for each user
+  private val movieAcc: Array[Double] = Array.fill(6){0}
+  private val movieCount: Array[Int] = Array.fill(6){0}
+  // Loop over all movies and compute the average rating for each genre.
+  for (movieID <- allMovies.indices if movieID != 0) {
+    val genres: Array[Boolean] = moviebase.genre(movieID)
+
+    for (g <- genres.indices) {
+      if (genres(g)) {
+        movieAcc(g) += userbase.compute_average_rating(movieID)
+        movieCount(g) += 1
+      }
+    }
   }
 
-  class Recommendations extends Heap[(String, Double)](1682, (x, y) => keyComp(y._2, x._2))
-
-  val mdb = new MovieBaseCDC
-  val udb = new Userbase_MM
-
-  //val mRatings: Array[Array[Int]] = udb.get_all_ratings()
-  val users = udb.get_all_users()
-
-  //for (movie <- mRatings) {mdb.addRating(movie(0), movie(1))} //Add movie ratings to mdb to get later when discerning genres.
-
-  var action: Int = 0
-  var noir: Int = 1
-  var light: Int = 2
-  var serious: Int = 3
-  var fantasy: Int = 4
-  var history: Int = 5
-
-  for (user <- users.indices if user != 0) {
-
+  // The recommendations are written to an output file
+  val writer = new FileWriter("output.txt", false)
+  // Loop over all users and print recommendations for each user
+  for (userID <- allUsers.indices if userID != 0) {
+    // Heap used to store a user's recommendation scores; convenient for popping at the end to get recommendations
     val rankings = new Recommendations()
+    // (Int, Int) = (movieID, rating)
+    val ratings: List[(Int, Int)] = allUsers(userID).get_users_ratings()
 
-    val id: Int = users(user).userID
-    val ratings: List[(Int, Int)] = users(id).get_users_ratings()
-
-
-    val genreAcc: Array[Double] = Array.fill(6){0}
-    val genreCount: Array[Double] = Array.fill(6){0}
-    val movieAcc: Array[Double] = Array.fill(6){0}
-    val movieCount: Array[Double] = Array.fill(6){0}
-
-
-
+    // Accumulates the user's total rating and number of ratings submitted for each genre
+    val genreAcc: Array[Int] = Array.fill(6){0}
+    val genreCount: Array[Int] = Array.fill(6){0}
+    // Loop over all of a user's ratings to calculate u_g
     for (rating <- ratings) {
-      val genres: Array[Boolean] = mdb.genre(rating._1)
+      val genres: Array[Boolean] = moviebase.genre(rating._1)
 
       for (g <- genres.indices) {
         if (genres(g)) {
           genreAcc(g) += rating._2
           genreCount(g) += 1
-          //movieAcc(g) += mdb.rating(rating._1)
-          movieAcc(g) += udb.compute_average_rating(rating._1)
-          movieCount(g) += 1
         }
       }
     }
 
+    // Stores the u_g, u_gAverage, and p_ug of all 6 genres for the current user
     val u_g: Array[Double] = (for (g <- genreAcc.indices) yield {genreAcc(g).toDouble / genreCount(g)}).toArray
-    val r_g: Array[Double] = (for (r <- movieAcc.indices) yield {movieAcc(r).toDouble / movieCount(r)}).toArray
-    val p_ug: Array[Double] = (for (p <- u_g.indices) yield {u_g(p) / r_g(p)}).toArray
+    val u_gAverage: Array[Double] = (for (g <- movieAcc.indices) yield {movieAcc(g) / movieCount(g)}).toArray
+    val p_ug: Array[Double] = (for (g <- u_g.indices) yield {u_g(g) / u_gAverage(g)}).toArray
 
-    val allMovies = mdb.getMovies
-
-    for (i <- allMovies.indices if i !=0) {
-      val g: Array[Boolean] = mdb.genre(i)
+    // Loop over all movies and generate a recommendation score for the user for each movie
+    // Then insert the recommendations in a maxHeap for sortedness
+    for (movieID <- allMovies.indices if movieID != 0) {
+      val genres: Array[Boolean] = moviebase.genre(movieID)
       var score: Double = 0
 
-      for (j <- g.indices if g(j)) {
-        score = math.max(score, udb.compute_average_rating(i) * p_ug(j))
+      for (j <- genres.indices if genres(j)) {
+        // Calculate a different recommendation score for each genre based on p_ug and take the maximum value
+        score = math.max(score, userbase.compute_average_rating(movieID) * p_ug(j))
       }
-
-      rankings.enqueue((allMovies(i).title, score))
+      rankings.enqueue((allMovies(movieID).title, score))
     }
-    for (x <- 0 to 1) println(rankings.pop())
-
+    // Pop the top 10, 30, 50, etc. recommendations from the heap
+    // Write the recommendations to an output file
+    for (x <- 0 until number_of_recommendations) {
+      writer.write(s"#${x+1} recommendation for user #${allUsers(userID).userID} is: ${rankings.pop()}\n")
+    }
   }
+  writer.close()
 }
